1ddef90cdba6717e5c7308a568f5430a3e147424
De-la-soul-is-dead <112073514+De-la-soul-is-dead@users.noreply.github.com>
2023-10-15 18:31:03 +0700

    add layout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard.index');
+});
+
+Route::get('/home', function () {
+    return view('client.layouts.app');
+});
+
+Route::get('/', [HomeController::class, 'index'])->name('client.home');
+
+Auth::routes();
+
